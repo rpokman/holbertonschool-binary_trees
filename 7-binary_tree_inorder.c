ae2e7f983773568370bd8ce0c7c370dbd3fc5ca6
rpokman <roymathis74200@gmail.com>
2025-08-04 13:40:15 +0200

    add functions 8 and fix betty for 7 and 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,11 +1,17 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_inorder - Traverses a binary tree using pre-order traversal
+* @tree: Pointer to the root node of the tree to traverse
+* @func: Pointer to a function to call for each node
+*/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(tree == NULL && func == NULL)
+	if (tree == NULL && func == NULL)
 	return;
 
+
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
