db966b00a89e275213bb8f6a63ac5ee8474fe058
rpokman <roymathis74200@gmail.com>
2025-08-04 10:55:51 +0200

    add all main.c and empty functions files

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+}
