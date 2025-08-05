02ae67d2ae4fff627f061491efcd2a209f913995
rpokman <roymathis74200@gmail.com>
2025-08-05 09:28:43 +0200

    fix: update documentation for binary_tree_leaves function to clarify its purpose

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_leaves - Measures the size of a binary tree.
+* @tree: Pointer to the root node of the tree to measure the size.
+* Return: Size of the tree (number of nodes), or 0 if tree is NULL.
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
